625bff384e8c5320483ce08c7543240cc34490f6
Breyten Ernsting <bje@dds.nl>
2011-04-20 17:00:48 +0200

    Add a settings file for easy configuration.

--- api.php
+++ api.php
@@ -1,22 +1,23 @@
 <?php
 
+include_once 'settings.php';
 include_once 'codebasehq.inc.php';
 
-$cb =& new CodeBaseHQAPI('eduhub', 'breyten', 'lykje2p55l2dgew4fnfkndtv4inqkscu8dpv77x3');
+$cb =& new CodeBaseHQAPI($codebaseAccount, $codebaseUser, $codebaseApikey);
 
 $what = $_GET['f'];
 
 if ($what == 'milestones') {
-    $milestones = $cb->get_milestones('www');
+    $milestones = $cb->get_milestones($codebaseMainProject);
     echo json_encode($milestones);
 }
 
 if ($what == 'statuses') {
-    $statuses = $cb->get_statuses('www');
+    $statuses = $cb->get_statuses($codebaseMainProject);
     echo json_encode($statuses);
 }
 
 if ($what == 'tickets') {
-    $tickets = $cb->search_tickets('www', sprintf('milestone:"%s"', $_GET['q']), $_GET['p']);
+    $tickets = $cb->search_tickets($codebaseMainProject, sprintf('sort:priority milestone:"%s"', $_GET['q']), $_GET['p']);
     echo json_encode($tickets);
 }
---
+++ settings.example.php
@@ -0,0 +1,6 @@
+<?php
+
+$codebaseAccount = 'account';
+$codebaseUser    = 'user';
+$codebaseApikey  = 'apikey';
+$codebaseMainProject = 'project';
