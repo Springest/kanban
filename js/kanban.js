e15121bf5c149c1f388848aae4ccef8bf5413d1d
Vincent Oord <vincent.oord@eduhub.nl>
2011-07-05 10:37:52 +0200

    Changed URL to ticket for new Codebase v4 urls

--- js/kanban.js
+++ js/kanban.js
@@ -36,7 +36,7 @@ function addTicket(ticket) {
     var userName = (users[ticket['assignee-id']] !== undefined) ? users[ticket['assignee-id']]['first-name'] + ' ' + users[ticket['assignee-id']]['last-name'] : '';
     $('#status-' + ticket['status-id']).append($('<div />').attr('id', ticketId).attr('class', 'ticket'));
     $('#' + ticketId).append($('<h3 />').attr('title', ticket.summary).text(ticket.summary.substr(0, 40) + ' ...'));
-    $('#' + ticketId).append($('<div class="ticket-link" />').append($('<a href="https://eduhub.codebasehq.com/www/tickets/' + ticket['ticket-id'] + '" target="_blank">#' + ticket['ticket-id'] + '</a>')));
+    $('#' + ticketId).append($('<div class="ticket-link" />').append($('<a href="https://eduhub.codebasehq.com/projects/www/tickets/' + ticket['ticket-id'] + '" target="_blank">#' + ticket['ticket-id'] + '</a>')));
     $('#' + ticketId).append($('<div class="ticket-priority" />').text(ticket.priority));
     if (gravatarHash != '') {
         $('#' + ticketId).append($('<img class="gravatar" src="http://www.gravatar.com/avatar/' + gravatarHash + '?s=16" title="' + userName + '" />'));
@@ -58,6 +58,7 @@ function countTickets() {
 $(document).ready(function() {
     $.get('/api.php?f=statuses', function (data) {
         statuses = data['ticketing-status'];
+
         $.each(statuses, function(i, status) {
             var status_class = (status['treat-as-closed'] == 'true') ? 'status-horizontal' : 'status-vertical';
             $('#statuses').append($('<div class="status ' + status_class + '" />').attr('id', 'status-' + status.id).append($('<h2 style="background: #'+ status['background-colour'] + ';" />').text(status.name)));
