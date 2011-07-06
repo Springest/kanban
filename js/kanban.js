623441a64704bcaf8b92bc79cbc4bfb521372d59
Yorick Sijsling <yoricksijsling@gmail.com>
2011-07-06 11:15:59 +0200

    Added last updated indication

--- js/kanban.js
+++ js/kanban.js
@@ -42,8 +42,22 @@ function addTicket(ticket) {
     var gravatarHash = (users[ticket['assignee-id']] !== undefined) ? users[ticket['assignee-id']].hash : '';
     var userName = (users[ticket['assignee-id']] !== undefined) ? users[ticket['assignee-id']]['first-name'] + ' ' + users[ticket['assignee-id']]['last-name'] : '';
 	var ticketSummary = (ticket.summary.length > 50) ? ticket.summary.substr(0, 50) + '...' : ticket.summary;
+	var minutesAgo = (new Date().getTime() - new Date(ticket['updated-at']).getTime()) / 60000;
+	var ageLong, ageShort;
+	if (minutesAgo > 24*60) {
+	    ageLong = Math.round(minutesAgo/24/60) + " days";
+	    ageShort = Math.round(minutesAgo/24/60) + "d";
+    } else if (minutesAgo > 60) {
+        ageLong = Math.round(minutesAgo/60) + " hrs";
+        ageShort = Math.round(minutesAgo/60) + "h";
+    } else {
+        ageLong = Math.round(minutesAgo) + " min";
+        ageShort = Math.round(minutesAgo) + "m";
+    }
+    
     $('#status-' + ticket['status-id']).append($('<div />').attr('id', ticketId).attr('class', 'ticket').attr('style', 'border-top: 2px solid ' + ticketPriority.colour + ';'));	
     $('#' + ticketId).append($('<h3 />').attr('title', ticket.summary).text(ticketSummary));
+    $('#' + ticketId).append($('<abbr class="age" title="updated '+ageLong+' ago">'+ageLong+'</abbr>'));
     $('#' + ticketId).append($('<a href="https://eduhub.codebasehq.com/projects/www/tickets/' + ticket['ticket-id'] + '" target="_blank" />').attr('class', 'ticket-link').text('#' + ticket['ticket-id']));
     if (gravatarHash != '') {
         $('#' + ticketId).append($('<img class="gravatar" src="http://www.gravatar.com/avatar/' + gravatarHash + '?s=32" title="' + userName + '" />'));
